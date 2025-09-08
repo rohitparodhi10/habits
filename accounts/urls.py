2c16317b0291ff9bc39218d93229b81ea4693fad
Rohit Parodhi <rohitparodhi599@gmail.com>
2025-09-08 18:29:01 +0530

    Add flipbook viewer to view pdf files in bookview

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from accounts.views import register_view, login_view, user_info, user_info_show
+from accounts.views import register_view, login_view, user_info, user_info_show, flipbook_view
 urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     path('info/', user_info, name='info'),
-    path('show/', user_info_show, name='show')
+    path('show/', user_info_show, name='show'),
+    path('flipbook/', flipbook_view, name='flipbook'),
 ]
